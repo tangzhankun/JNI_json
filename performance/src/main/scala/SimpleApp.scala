b817467e566bc29e48bde2907e549635eee5ab2d
tangzhankun <zhankun.tang@intel.com>
2018-03-09 02:30:52 +0800

    micro-benchmark sql app

--- performance/src/main/scala/SimpleApp.scala
+++ performance/src/main/scala/SimpleApp.scala
@@ -1,11 +1,18 @@
 import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
-  def main(args: Array[String]) {
+  def micoBenchmark(spark : SparkSession): Unit = {
     val jsonFile = "./Small.json" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Micro Benchmark of FPGA JSON IP").getOrCreate()
-    val peopleDF = spark.read.format("json").load(jsonFile)
-    peopleDF.show()
+
+    val smallDF = spark.read.format("json").load(jsonFile)
+    smallDF.show()
+    smallDF.createOrReplaceTempView("gdi_mb")
+    val ret = spark.sql("select count(OPER_TID), count(NBILLING_TID), count(OBILLING_TID), count(ACC_NBR) from gdi_mb")
+    ret.show()
+  }
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Micro Benchmark of FPGA JSON IP").master("local[1]").getOrCreate()
+    micoBenchmark(spark)
     spark.stop()
   }
 }
