167747f83fe1ee335571c84fedceb9b90368b439
tangzhankun <tangzhankun@gmail.com>
2018-03-13 12:59:08 +0800

    micro-benchmark.sh

--- performance/micro-benmark.sh
+++ performance/micro-benmark.sh
@@ -3,4 +3,4 @@ export SPARK_HOME=/home/wasaisw1/forIntel/spark_integration/spark
 $SPARK_HOME/bin/spark-submit \
   --class "SimpleApp" \
   --master local[1] \
-  target/scala-2.11/fpga-json-performance_2.11-1.0.jar datagen 1000 ./1000row.json
+  target/scala-2.11/fpga-json-performance_2.11-1.0.jar micro ./1000row.json false
