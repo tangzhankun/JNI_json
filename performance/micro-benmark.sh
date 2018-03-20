54dacc82ee07e1829b5547bf2c49f8a51a1db7ba
tangzhankun <zhankun.tang@intel.com>
2018-03-21 00:24:53 +0800

    modify the micro-benchmark.sh to fit the more columns JSON file

--- performance/micro-benmark.sh
+++ performance/micro-benmark.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export SPARK_HOME=/home/wasaisw1/forIntel/spark_integration/spark
+export SPARK_HOME=/root/code/spark
 $SPARK_HOME/bin/spark-submit \
   --class "SimpleApp" \
   --master local[1] \
-  --driver-memory 2G \
-  --executor-memory 2G\
-  target/scala-2.11/fpga-json-performance_2.11-1.0.jar micro ./1000000-5row.json true
+  --driver-memory 6G \
+  target/scala-2.11/fpga-json-performance_2.11-1.0.jar micro ./10000000-row-37col.json false
