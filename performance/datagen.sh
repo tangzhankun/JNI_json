3c3b6b9ff31a7b6e462e70e7ff87133bde2fad91
tangzhankun <tangzhankun@gmail.com>
2018-03-28 22:24:05 +0800

    add new datagen and sql aggregation script

--- performance/datagen.sh
+++ performance/datagen.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-export SPARK_HOME=/root/code/spark
-lines=10
-$SPARK_HOME/bin/spark-submit \
-  --class "SimpleApp" \
-  --master local[1] \
-  target/scala-2.11/fpga-json-performance_2.11-1.0.jar datagen $lines ./$lines-row-37col.json 5
+export SPARK_HOME=/home/wasaisw1/forIntel/spark_integration/spark
+for chars in 30
+do
+  #for lines in 10 100 1000 10000 100000 1000000 10000000
+  for lines in 1500000
+  do
+  echo "Generating $lines JSON file with 37 colums($chars length field value) each line"
+  $SPARK_HOME/bin/spark-submit \
+    --class "SimpleApp" \
+    --master local[1] \
+    target/scala-2.11/fpga-json-performance_2.11-1.0.jar datagen $lines ./$lines-row-37col-$chars-char.json $chars
+  sleep 5
+  done
+done
--- performance/sql-agg-benchmark.sh
+++ performance/sql-agg-benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export SPARK_HOME=/root/code/spark
+export SPARK_HOME=/home/wasaisw1/forIntel/spark_integration/spark
 $SPARK_HOME/bin/spark-submit \
   --class "SimpleApp" \
   --master local[1] \
   --driver-memory 6G \
-  target/scala-2.11/fpga-json-performance_2.11-1.0.jar end-to-end ./100-row-37col.json false
+  target/scala-2.11/fpga-json-performance_2.11-1.0.jar sql-agg ./BigReal.json false
