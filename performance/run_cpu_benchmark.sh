58780f998afa5e583ef296f41e322705df316dc9
tangzhankun <zhankun.tang@intel.com>
2018-03-29 23:43:43 +0800

    fit the cpu micro benchark to set the warm up flag

--- performance/run_cpu_benchmark.sh
+++ performance/run_cpu_benchmark.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # you need to use the tangzhankun' spark branch cpujson_benchmark
 # https://github.com/tangzhankun/spark.git
+# the last boolean is indicate if you want to warm up
 
 export SPARK_HOME=/root/code/spark
-$SPARK_HOME/bin/spark-submit \
+nice -n -15 $SPARK_HOME/bin/spark-submit \
   --class "org.apache.spark.examples.sql.SparkSQLExample" \
   --master local[1] \
   --driver-memory 6G \
-  $SPARK_HOME/examples/target/scala-2.11/jars/spark-examples_2.11-2.2.1-SNAPSHOT.jar /root/customer_support/WASAI/performance/10000000-5row.json
+  $SPARK_HOME/examples/target/scala-2.11/jars/spark-examples_2.11-2.2.1-SNAPSHOT.jar /root/customer_support/WASAI/performance/1500000-row-37col-30-char.json true
