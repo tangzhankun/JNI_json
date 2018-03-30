dfeaa8a8092b9962e88e7d93b3e2707a04f039d7
tangzhankun <tangzhankun@gmail.com>
2018-03-30 16:19:00 +0800

    add FPGA and CPU micro-benchmark scripts against multiple files

--- performance/run_cpu_benchmark.sh
+++ performance/run_cpu_benchmark.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # you need to use the tangzhankun' spark branch cpujson_benchmark
 # https://github.com/tangzhankun/spark.git
+export SPARK_HOME=/home/wasaisw1/forIntel/spark_integration/spark_micro
 
-export SPARK_HOME=/root/code/spark
+declare -a filePaths=(
+		"/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/10-row-37col-30-char.json"
+               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/100-row-37col-30-char.json"
+#               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/1000-row-37col-30-char.json"
+#               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/10000-row-37col-30-char.json"
+#               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/100000-row-37col-30-char.json"
+#               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/1000000-row-37col-30-char.json"
+               "/home/wasaisw1/forIntel/spark_integration/row_from_FPGA/JNI_json/performance/1500000-row-37col-30-char.json"
+               )
+for path in "${filePaths[@]}"
+do
+
 $SPARK_HOME/bin/spark-submit \
   --class "org.apache.spark.examples.sql.SparkSQLExample" \
   --master local[1] \
   --driver-memory 6G \
-  $SPARK_HOME/examples/target/scala-2.11/jars/spark-examples_2.11-2.2.1-SNAPSHOT.jar /root/customer_support/WASAI/performance/10000000-5row.json
+  $SPARK_HOME/examples/target/scala-2.11/jars/spark-examples_2.11-2.2.1-SNAPSHOT.jar $path false
+done
